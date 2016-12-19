be5b010b73f376bde9ad5a9ee82c1a012f5d2d68
Daanniello <daansmits_@hotmail.com>
2016-12-19 11:00:28 +0100

    MEER PLUSJES

--- Connection_database.php
+++ Connection_database.php
@@ -1,2 +1,19 @@
 <?php
-?>
+
+$DBConnect = mysqli_connect("127.0.0.1", "root", "");
+if ($DBConnect === FALSE)
+{
+    echo "<p>Unable to connect to the database server.</p>"
+    . "<p>Error code " . mysqli_errno() . ": " . mysqli_error()
+    . "</p>";
+} else
+{
+    $DBName = "";
+
+    $TableName = "";
+    $SQLstring = "SELECT * FROM $TableName";
+    $QueryResult = mysqli_query($DBConnect, $SQLstring);
+    mysqli_free_result($QueryResult);
+
+    mysqli_close($DBConnect);
+}
