04256a29ce39dbe05a0c8e8ad098d2610260c2ba
Daanniello <daansmits_@hotmail.com>
2016-12-20 11:47:33 +0100

    form verbetering

--- Portfolio.php
+++ Portfolio.php
@@ -4,20 +4,23 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<?php include "includes/topinclude.php";?>
-        <div class="project_upload">
-        <form action="#" method="post">
-            <div class="project_upload_title">
-                Projecten
-                <input type="file" name="upload_project">
-                <input type="submit" name="submit">
-            </div>
-            <div class="project_upload_file">
-                <?php 
-                include "connection_database.php";
-                ?>
-            </div>
-            
-        </form>
+<?php include "includes/topinclude.php"; ?>
+<div class="project_upload">
+    <form action="#" method="post">
+        <div class="project_upload_title_project">
+            Projecten
+
         </div>
+        <div class="project_upload_title">
+            <input type="file" name="upload_project">
+            <p><input type="submit" name="submit" value="upload" ></p>
+        </div>
+        <div class="project_upload_file">
+            <?php
+            include "connection_database.php";
+            ?>
+        </div>
+
+    </form>
+</div>
 <?php include "includes/botinclude.php"; ?>
