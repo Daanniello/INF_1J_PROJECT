eb1e72f4c11155fd12b07e6d688e6378d7c03f54
Daanniello <daansmits_@hotmail.com>
2016-12-20 10:52:46 +0100

    Project_upload

--- Portfolio.php
+++ Portfolio.php
@@ -5,8 +5,19 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <?php include "includes/topinclude.php";?>
-        
+        <div class="project_upload">
         <form action="#" method="post">
+            <div class="project_upload_title">
+                Projecten
+                <input type="file" name="upload_project">
+                <input type="submit" name="submit">
+            </div>
+            <div class="project_upload_file">
+                <?php 
+                include "connection_database.php";
+                ?>
+            </div>
             
         </form>
+        </div>
 <?php include "includes/botinclude.php"; ?>
