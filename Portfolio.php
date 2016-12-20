6d1d6358788f9d3d6f2007d9ebd0445d85b9d98d
Daanniello <daansmits_@hotmail.com>
2016-12-20 10:37:25 +0100

    KOEL

--- Portfolio.php
+++ Portfolio.php
@@ -4,13 +4,9 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<?php include "";?>
+<?php include "includes/topinclude.php";?>
         
         <form action="#" method="post">
             
         </form>
-        <?php
-        
-        ?>
-    </body>
-</html>
+<?php include "includes/botinclude.php"; ?>
