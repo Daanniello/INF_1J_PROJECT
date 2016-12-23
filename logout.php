6e0b5baee3a166f25506bbce35ca6f4b9952ead2
Daanniello <daansmits_@hotmail.com>
2016-12-23 11:38:13 +0100

    Instant logout fix

--- logout.php
+++ logout.php
@@ -1,10 +1,17 @@
 <?php include "includes/topinclude.php"; ?>
-	<div class="inhoud">
-		<?php 
-			session_destroy();
-			echo "you have been logged out";
-		?>
-	</div>
+<div class="inhoud">
+    <?php
+    if (isset($_SESSION["username"]))
+    {
+        session_destroy();
+        header("Refresh:0");
+    } else
+    {
+
+        echo "you have been logged out";
+    }
+    ?>
+</div>
 <?php include "includes/botinclude.php"; ?>
 
 
