db9e0826128515db29b0c7545a27a991ff19a5a3
Daanniello <daansmits_@hotmail.com>
2017-01-09 16:16:31 +0100

    portfolio

--- Portfolio.php
+++ Portfolio.php
@@ -1,7 +1,8 @@
 <?php
-	include "includes/topinclude.php";
-	include "connection_database.php";
+include "includes/topinclude.php";
+include "connection_database.php";
 ?>
+
 <div class="inhoud">
     <div class="project_upload">
         <form action="portfolio.php" method="post">
@@ -17,12 +18,36 @@
 					<input type="file" name="upload_project">
 					<p><input type="submit" name="submit" value="upload" ></p>
 				</div>';
-            } else {
+            } else
+            {
                 echo "<div class='project_upload_title'> you have to log in first to edit this file </div>";
             }
             ?>
+            
             <div class="project_upload_file">
-                <?php ?>
+                <?php
+            if (isset($_POST['submit']))
+            {
+                if (empty($_POST['upload_project']))
+                {
+                    echo "Je hebt geen bestand gekozen.";
+                } else
+                {
+                    $file= $_POST['upload_project'];
+                    echo $file;
+                }
+            }
+            ?>
+                <?php
+                if (isset($_SESSION['username']))
+                {
+                    require "Connection_database.php";
+                } else
+                {
+                    echo "Je moet eerst inloggen";
+                }
+                ?>
+
             </div>
         </form>
     </div>
@@ -33,19 +58,44 @@
                 SLB Projecten
             </div>
             <?php
-            if (isset($_SESSION["login_user"]) == 1) {
+            if (isset($_SESSION["login_user"]) == 1)
+            {
                 echo '
         
 				<div class="project_upload_title">
-					<input type="file" name="upload_project">
-					<p><input type="submit" name="submit" value="upload" ></p>
+					<input type="file" name="upload_project1">
+					<p><input type="submit" name="submit1" value="upload" ></p>
 				</div>';
-            } else {
+            } else
+            {
                 echo "<div class='project_upload_title'> you have to log in first to edit this file </div>";
             }
             ?>
+            
             <div class="project_upload_file">
-                <?php ?>
+                <?php
+                
+            if (isset($_POST['submit1']))
+            {
+                 if (empty($_POST['upload_project1']))
+                {
+                    echo "Je hebt geen bestand gekozen.";
+                } else
+                {
+                    $file=$_POST['upload_project1'];
+                    echo $file;
+                }
+            }
+            ?>
+                <?php
+                if (isset($_SESSION['username']))
+                {
+                    echo "";
+                } else
+                {
+                    echo "Je moet eerst inloggen";
+                }
+                ?>
             </div>
         </form>
     </div>
