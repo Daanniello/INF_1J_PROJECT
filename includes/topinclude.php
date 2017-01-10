a5d2fd13963c85edcc29db35fbfb0bfcb6dd64e7
xXLars007Xx <larskuizenga@live.nl>
2017-01-10 13:51:39 +0100

    Register niet wanneer ingelogd

--- includes/topinclude.php
+++ includes/topinclude.php
@@ -30,9 +30,10 @@ and open the template in the editor.
 								echo "<li class='li_opmaak'><a href='logout.php' class='a_opmaak' >Log out</a></li>";
 							} else {
 								echo "<li class='li_opmaak'><a href='login.php' class='a_opmaak'>Log in </a></li>";
-							}
-								echo "<li class='li_opmaak'><a href='portfolio.php' class='a_opmaak'>Portfolio </a></li>";
 								echo "<li class='li_opmaak'><a href='register.php' class='a_opmaak'>Register </a></li>";
+							}
+							echo "<li class='li_opmaak'><a href='portfolio.php' class='a_opmaak'>Portfolio </a></li>";
+								
 							
                         echo "</ul>";
                     echo "</div>";
