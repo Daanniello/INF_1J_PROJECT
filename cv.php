f6f2623cde88b073d84e60be56d0bc88cbd8e0df
Daanniello <daansmits_@hotmail.com>
2017-01-11 15:47:32 +0100

    CV DOET HET !!!

--- cv.php
+++ cv.php
@@ -17,22 +17,23 @@
         $count2 = mysqli_num_rows($result2);
         if ($count2 == 1)
         {
-            
+            echo "hoi";
         } else
         {
             echo "Je hebt nog geen CV, Upload hem nu.";
-            echo "<form enctype='multipart/form-data' action='#' method='post'><input type='submit' name='submit' value='upload cv' ></form> ";
+            echo "<form enctype='multipart/form-data' action='#' method='post'><input type='file' name='upload' value='CV'> <input type='submit' name='submit' value='upload cv' ></form> ";
             if (isset($_POST['submit']))
             {
-                if (empty($FILES['upload']))
+                if (empty($_FILES['upload']))
                 {
                     echo "Je hebt geen bestand gekozen.";
                 } else
                 {
-                    
+                    $query = "INSERT INTO cv (CV_ID,Link,StudentNummer) VALUES(NULL,'{$_SESSION['username']}','$studentnumber')";
+                    mysqli_query($DBConnect, $query);
                     $target_path = "includes/CV/";
 
-                    $target_path = $target_path . $_SESSION;
+                    $target_path = $target_path . $_SESSION['username'] . substr(basename($_FILES['upload']['name']), strrpos(basename($_FILES['upload']['name']), "."), 5);
 
                     if (move_uploaded_file($_FILES['upload']['tmp_name'], $target_path))
                     {
