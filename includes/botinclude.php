51bac0ac45b0d0663c613ae5f73ec535977eac09
gumgum121 <daan.smit1@student.stenden.com>
2017-01-11 11:59:41 +0100

    test

--- includes/botinclude.php
+++ includes/botinclude.php
@@ -1,6 +1,19 @@
-			<div class="footer">
-			
-            </div>
-        </div>
-    </body>
+<div class="footer">
+    <div class="school">
+        <b>Stenden Hogeschool</b> | <i>2008</i>
+    </div>
+    <div class="link">
+        <p><a href="https://outlook.office.com/owa/">Student mail</a></p>
+        Employee mail
+        Stenden Desktop (U and G drive)
+        Schedules
+        Blackboard
+        Students Charter & TER
+        Studentenstatuut & OER
+        Downloads for students
+        Downloads voor studenten
+    </div>	
+</div>
+</div>
+</body>
 </html>
