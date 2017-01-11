c05c935af2447c65f3b2f632e6884e720aa89df3
gumgum121 <daan.smit1@student.stenden.com>
2017-01-11 15:41:58 +0100

    home bijna klaar

--- Index.php
+++ Index.php
@@ -1,10 +1,13 @@
 <?php include "includes/topinclude.php"; ?>
 <div class="inhoud">
-    <div class="homeText">
+    <div class="homeTitel">
         <h1>Welkom bij het Portfolio van Stenden.</h1>
     </div>
     <div class="homeFoto">
         <img src="includes/images/home.jpg">
+    </div>
+    <div class="homeTekst">
+        hier komen comments van gebruikers.
     </div>
     <div class="clear"></div>
 </div>
