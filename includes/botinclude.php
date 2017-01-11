5d2b5ef6b30b76eaee34f7b8d0d35c9f56a4b14e
gumgum121 <daan.smit1@student.stenden.com>
2017-01-11 12:10:36 +0100

    test clear

--- includes/botinclude.php
+++ includes/botinclude.php
@@ -4,14 +4,14 @@
     </div>
     <div class="link">
         <p><a target="blank" href="https://outlook.office.com/owa/">Student mail</a></p>
-        <p><a target="blank" href="">Employee mail</a></p>
-        <p><a target="blank" href="">Stenden Desktop (U and G drive)</a></p>
-        <p><a target="blank" href="">Schedules</a></p>
-        <p><a target="blank" href="">Blackboard</a></p>
-        <p><a target="blank" href="">Students Charter & TER</a></p>
-        <p><a target="blank" href="">Studentenstatuut & OER</a></p>
-        <p><a target="blank" href="">Downloads for students</a></p>
-        <p><a target="blank" href="">Downloads voor studenten</a></p>
+        <p><a target="blank" href="https://outlook.office.com/owa/">Employee mail</a></p>
+        <p><a target="blank" href="https://ra.stenden.com/RDWeb/pages/en-US/login.aspx?ReturnUrl=default.aspx">Stenden Desktop (U and G drive)</a></p>
+        <p><a target="blank" href="https://adfs.stenden.com/adfs/ls/?wa=wsignin1.0&wtrealm=urn%3awww.istenden.com%3aadfs&wctx=https%3a%2f%2fwww.istenden.com%2fportal%2fstudent%2froos%2f_layouts%2fAuthenticate.aspx%3fSource%3d%252Fportal%252Fstudent%252Froos%252FPaginas%252FDefault%252Easpx">Schedules</a></p>
+        <p><a target="blank" href="https://elo.stenden.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1">Blackboard</a></p>
+        <p><a target="blank" href="https://www.istenden.com/Pages/Students-Charter-and-Teaching-and-Examination-Regulation-.aspx">Students Charter & TER</a></p>
+        <p><a target="blank" href="https://www.istenden.com/Pages/Studentenstatuut-en-Onderwijs--en-Examenregeling.aspx">Studentenstatuut & OER</a></p>
+        <p><a target="blank" href="https://www.istenden.com/Pages/Downloads-English.aspx">Downloads for students</a></p>
+        <p><a target="blank" href="https://www.istenden.com/Pages/Downloads.aspx">Downloads voor studenten</a></p>
     </div>	
 </div>
 </div>
