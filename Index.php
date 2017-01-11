166306f67de7f68c71f391dd1552122e95549585
gumgum121 <daan.smit1@student.stenden.com>
2017-01-11 15:13:21 +0100

    chick

--- Index.php
+++ Index.php
@@ -1,9 +1,12 @@
 <?php include "includes/topinclude.php"; ?>
 <div class="inhoud">
-    
     <div class="homeText">
-        
+        <h1>Welkom bij het Portfolio van Stenden.</h1>
     </div>
+    <div class="homeFoto">
+        <img src="includes/images/home.jpg">
+    </div>
+    <div class="clear"></div>
 </div>
 <?php include "includes/botinclude.php"; ?>
             
