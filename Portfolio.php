4cc8b7a44f3070f6611dc2158b08dd2b9483f8c9
Daanniello <daansmits_@hotmail.com>
2017-01-16 14:32:55 +0100

    PUSHSHSHSHHSHSHSHSH THEE BESCHRIJVING

--- Portfolio.php
+++ Portfolio.php
@@ -24,21 +24,21 @@ include "connection_database.php";
                 echo "<div class='project_upload_title'> you have to log in first to edit this file </div>";
             }
             ?>
-            
+
             <div class="project_upload_file">
                 <?php
-            if (isset($_POST['submit']))
-            {
-                if (empty($_POST['upload_project']))
+                if (isset($_POST['submit']))
                 {
-                    echo "Je hebt geen bestand gekozen.";
-                } else
-                {
-                    $file= $_POST['upload_project'];
-                    echo $file;
+                    if (empty($_POST['upload_project']))
+                    {
+                        echo "Je hebt geen bestand gekozen.";
+                    } else
+                    {
+                        $file = $_POST['upload_project'];
+                        echo $file;
+                    }
                 }
-            }
-            ?>
+                ?>
                 <?php
                 if (isset($_SESSION['username']))
                 {
@@ -69,22 +69,21 @@ include "connection_database.php";
                 echo "<div class='project_upload_title'> you have to log in first to edit this file </div>";
             }
             ?>
-            
+
             <div class="project_upload_file">
                 <?php
-                
-            if (isset($_POST['submit1']))
-            {
-                 if (empty($_POST['upload_project1']))
+                if (isset($_POST['submit1']))
                 {
-                    echo "Je hebt geen bestand gekozen.";
-                } else
-                {
-                    $file=$_POST['upload_project1'];
-                    echo $file;
+                    if (empty($_POST['upload_project1']))
+                    {
+                        echo "Je hebt geen bestand gekozen.";
+                    } else
+                    {
+                        $file = $_POST['upload_project1'];
+                        echo $file;
+                    }
                 }
-            }
-            ?>
+                ?>
                 <?php
                 if (isset($_SESSION['username']))
                 {
@@ -93,6 +92,38 @@ include "connection_database.php";
                 ?>
             </div>
         </form>
+    </div>
+    <div class="beschrijving">
+        <?php
+        if (isset($_SESSION['username']))
+        {
+            
+            require 'Connection_database.php';
+            $get = "SELECT beschrijving FROM student WHERE GebruikerID = '{$_SESSION['id']}' ";
+            $query = mysqli_query($DBConnect, $get);
+            $row = mysqli_fetch_assoc($query);
+            
+            if (mysqli_num_rows($query)!== 1)
+            {
+                echo "<form action='#' method='post'><textarea name='area' ></textarea><input type='submit' name='submit2' value='Voeg toe'> </form> ";
+                if (isset($_POST['submit2'])){
+                    $vul = $_POST['area'];
+                    $querie = "UPDATE student SET beschrijving = '$vul' WHERE GebruikerID = '{$_SESSION['id']}' ";
+                    mysqli_query($DBConnect, $querie);
+                    echo "query send";
+                }
+            } else
+            {
+
+
+                echo $row['beschrijving'];
+                
+            }
+        } else
+        {
+            
+        }
+        ?> 
     </div>
 </div>
 <?php include "includes/botinclude.php"; ?>
