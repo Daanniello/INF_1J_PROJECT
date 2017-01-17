2431909092a60327f35cc3e770132f8be7c57fcf
Daanniello <daansmits_@hotmail.com>
2017-01-17 12:55:32 +0100

    DOCENTEN KUNNEN NU BIJ JOU SPIONEREN!

--- studentfile.php
+++ studentfile.php
@@ -4,10 +4,16 @@
         if (isset($_SESSION['login_docent'])){
             require 'connection_database.php';
             $get = "SELECT * FROM cv JOIN gebruiker WHERE StudentNummer = '{$_GET['student']}' OR StudentNummer = '{$_GET['student']}'";
+            $get1 = "SELECT * FROM project JOIN gebruiker ON project.StudentNummer = gebruiker.GebruikerID  WHERE StudentNummer = '{$_GET['student']}' ";
             $query = mysqli_query($DBConnect, $get);
+            $query1 = mysqli_query($DBConnect, $get1);
             $row = mysqli_fetch_assoc($query);
-            echo "CV: <a href='includes/CV/{$row['Gebruiker']}.{$row['Link']}'> {$row['Gebruiker']}_CV</a>";
-            echo "";
+            
+            echo "CV:<br> <a href='includes/CV/{$row['Gebruiker']}.{$row['Link']}'> {$row['Gebruiker']}_CV</a><br>";
+            echo "<br>Project:<br>";
+            while ($row1 = mysqli_fetch_assoc($query1)){
+                echo "<a href='includes/project/{$row1['Naam']}{$row1['Gebruiker']}.{$row1['Project']}' target='blank'>{$row1['Naam']}</a><br>";
+            }
         }
         ?>
            
