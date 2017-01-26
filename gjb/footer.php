4d644e8f36125c6c768f72c55c20e2d36891abbd
GertJanB10 <gertjanboertien@gmail.com>
2017-01-26 11:55:48 +0100

    old stuff

--- gjb/footer.php
+++
@@ -1,8 +0,0 @@
-        </div>
-    </div>
-    <div class="footer">
-        <div class="ftext">
-            ©2017 Stenden/<?php echo $name; ?>. All Rights Reserved.
-        </div>
-    </div>
-</body>
